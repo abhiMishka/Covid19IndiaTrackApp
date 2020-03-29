ea39a9c762a534fe94f49fa5bbd685dd47f411c4
abhishek <abhishek.k@loconav.com>
2020-03-29 19:53:13 +0530

    added internet connectivity check

--- app/src/main/java/com/example/bottomnavigation/ui/SplashActivity.kt
+++ app/src/main/java/com/example/bottomnavigation/ui/SplashActivity.kt
@@ -2,8 +2,11 @@ package com.example.bottomnavigation.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.example.bottomnavigation.R
+import com.example.bottomnavigation.helper.UtilFunctions
 import com.example.bottomnavigation.network.DataManager
+import kotlinx.android.synthetic.main.activity_splash.*
 import kotlinx.android.synthetic.main.layout_loading.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,7 +17,25 @@ class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        rotateClockwise(appIconIv)
+        inflateUI()
+    }
+
+    private fun inflateUI() {
+        laodingTv.text = getString(R.string.loading)
+        retryButton.visibility = View.GONE
+        if (UtilFunctions.isNetworkAvailable()) {
+            rotateClockwise(appIconIv)
+            getData()
+        } else {
+            retryButton.visibility = View.VISIBLE
+            laodingTv.text = getString(R.string.no_internet_connection)
+            retryButton.setOnClickListener {
+                inflateUI()
+            }
+        }
+    }
+
+    private fun getData() {
         GlobalScope.launch {
             DataManager.syncData()
             rotate?.pause()
@@ -23,7 +44,7 @@ class SplashActivity : BaseActivity() {
         }
     }
 
-    fun openMainActivity() {
+    private fun openMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
         startActivity(intent)
